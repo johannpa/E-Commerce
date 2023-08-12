a30528e07063cdf4fbd65ae3a40852bf42ed4582
johannpa <johann_paque@hotmail.fr>
2023-08-12 18:00:01 +0200

    E-Commerce App Create model, create connection string

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
