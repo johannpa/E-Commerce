59212617dca2c2921fb6eed0d22a1a763aa92b4b
johannpa <johann_paque@hotmail.fr>
2023-08-16 18:27:09 +0200

    Add TempData to display message for all the methods

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -36,6 +36,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
           return View(obj);
@@ -71,6 +72,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -105,6 +107,7 @@ namespace BulkyBookWeb.Controllers
             
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         
         }
